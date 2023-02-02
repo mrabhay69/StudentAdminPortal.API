da2280bc0c2751869c6db3eb8152e1ba6098f83c
mrabhay69 <abhaymaurya245@gmail.com>
2023-02-02 15:31:45 +0530

    End Of Section 6

--- StudentAdminPortal.API/StudentAdminPortal.API/Repositories/IStudentRepository.cs
+++ StudentAdminPortal.API/StudentAdminPortal.API/Repositories/IStudentRepository.cs
@@ -13,5 +13,7 @@ namespace StudentAdminPortal.API.Repositories
         Task<bool> Exists(Guid studentId);
 
         Task<Student> UpdateStudent(Guid studentId, Student request);
+
+        Task<Student> DeleteStudent(Guid studentId);
     }
 }
